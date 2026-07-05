3e155e80693d54879207724b5d6c8236c3b1e2f9
Lalit Singh <nlalitsingh2014@gmail.com>
2026-07-05 09:12:39 +0545

    Putting database  url string to env file thought dotenv

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,12 @@
 //In here we connect database  and code logic 
+require("dotenv").config();
  const {Sequelize,DataTypes,} = require("sequelize")
 // we also write the above like  this 
 //  const sequelize = require("sequelize");
 //  const Sequelize = sequelize.Sequelize
 //  const DataTypes = sequelize.DataTypes
 
-const sequelize = new Sequelize("postgresql://postgres.auuyhugiorytfkpazpfc:Pramod@5929@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres")
+const sequelize = new Sequelize(process.env.DATABASE_URL,{dialect:"postgres",logging:false})
 sequelize.authenticate()
 .then(()=>{
     console.log("Authonticate Successfully Done,connected !!")
